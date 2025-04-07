03e6728f58770f5775c9e391e89b1550b6c49771
johnny-emp <johnny@empyrealsdk.com>
2025-04-07 00:29:14 -0700

    comment quickstart

--- {{cookiecutter.project_slug}}/python_ai_agent/quickstart.sh
+++ {{cookiecutter.project_slug}}/python_ai_agent/quickstart.sh
@@ -1,8 +1,13 @@
 export DEPLOYMENT_FILESYSTEM_PATH=.
+
+# Run migrations
 alembic revision --autogenerate -m "initial migration"
 alembic upgrade head
+
+# Install dependencies
 poetry install
 poetry lock
 
+# Run the agent
 echo "Starting {{ cookiecutter.project_name }}"
 poetry run {{ cookiecutter.project_slug }}

f0fc7d18213b57757d9628a8872344f595f958ae
defer logic <defer_logic@proton.me>
2025-04-11 11:19:28 +0300

    init service and fix dockerfile

--- python_ai_agent/{{cookiecutter.project_slug}}/quickstart.sh
+++ python_ai_agent/{{cookiecutter.project_slug}}/quickstart.sh
@@ -1,9 +1,9 @@
 export DEPLOYMENT_FILESYSTEM_PATH=.
 
-# Run migrations
-alembic revision --autogenerate -m "initial migration"
-alembic upgrade head
-
 # Install dependencies
 poetry install
 poetry lock
+
+# Run migrations
+alembic revision --autogenerate -m "initial migration"
+alembic upgrade head
--- x_agent/{{cookiecutter.project_slug}}/quickstart.sh
+++ x_agent/{{cookiecutter.project_slug}}/quickstart.sh
@@ -1,3 +1,7 @@
 # Install dependencies
 poetry install
 poetry lock
+
+# Run migrations
+alembic revision --autogenerate -m "initial migration"
+alembic upgrade head

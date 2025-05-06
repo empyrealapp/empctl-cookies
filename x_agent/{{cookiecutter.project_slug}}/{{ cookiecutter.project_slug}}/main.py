d0f35945a626b2a2307063856e2428046bb4c0b2
johnny-emp <164106913+johnny-emp@users.noreply.github.com>
2025-05-05 19:36:04 -0700

    Update main.py

--- x_agent/{{cookiecutter.project_slug}}/{{ cookiecutter.project_slug}}/main.py
+++ x_agent/{{cookiecutter.project_slug}}/{{ cookiecutter.project_slug}}/main.py
@@ -1,14 +1,13 @@
-from datetime import datetime, timezone
 import os
+from datetime import datetime, timezone
 
 from emp_agents import AgentBase
-from emp_agents.providers import OpenAIProvider, OpenAIModelType
+from emp_agents.providers import OpenAIModelType, OpenAIProvider
+from emp_hooks import log, manager, scheduler
+from emp_hooks.handlers import twitter
 from tweepy import Tweet
 from tweepy.client import Client
 
-from emp_hooks import scheduler, log
-from emp_hooks.handlers import twitter
-from emp_hooks import manager
 from .prompts import MAIN_PROMPT
 
 agent = AgentBase(
@@ -16,6 +15,7 @@ agent = AgentBase(
     prompt=MAIN_PROMPT,
     sync_tools=True,
 )
+
 
 @twitter.on_tweet(os.environ["TWITTER_QUERY"])
 async def on_simmi_tweet(tweet: Tweet) -> bool:
